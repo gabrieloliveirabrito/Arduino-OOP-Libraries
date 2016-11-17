99ee0664edb620d1f1fedb66bad647749923a478
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2016-11-16 23:08:04 -0200

    Add files via upload

---
+++ Includes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Base\Defines.h"
+#include "Collections\List.hpp"
+
+//Bases
+#include "Base\IComponent.h"
+#include "Base\Pin.h"
+
+//Classes
+#include "Components\LED.h"
+#include "Components\LDR.h"
+#include "Components\Buzzer.h"
+#include "Components\Button.h"
+#include "Components\Potentiometer.h"
